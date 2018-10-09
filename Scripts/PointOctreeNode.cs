1b14cc9a608bbf931c3655184b303a1dcc0810e6
Bill B <nition@users.noreply.github.com>
2018-10-09 22:14:29 +1300

    Keep things working in Unity 4
    Keep things working in Unity 4, which doesn't have the bounds.ClosestPoint check.

--- Scripts/PointOctreeNode.cs
+++ Scripts/PointOctreeNode.cs
@@ -153,11 +153,24 @@ public class PointOctreeNode<T> {
 	/// <param name="result">List result.</param>
 	/// <returns>Objects within range.</returns>
 	public void GetNearby(ref Vector3 position, float maxDistance, List<T> result) {
-        // Does the node intersects with the sphere of center = position and radius = maxDistance?
         float sqrMaxDistance = maxDistance * maxDistance;
-        if ((bounds.ClosestPoint(position) - position).sqrMagnitude > sqrMaxDistance) {
+
+#if UNITY_2017_1_OR_NEWER
+		// Does the node intersect with the sphere of center = position and radius = maxDistance?
+		if ((bounds.ClosestPoint(position) - position).sqrMagnitude > sqrMaxDistance) {
             return;
         }
+#else
+		// Does the ray hit this node at all?
+		// Note: Expanding the bounds is not exactly the same as a real distance check, but it's fast
+		// TODO: Does someone have a fast AND accurate formula to do this check?
+		bounds.Expand(new Vector3(maxDistance * 2, maxDistance * 2, maxDistance * 2));
+		bool contained = bounds.Contains(position);
+		bounds.size = actualBoundsSize;
+		if (!contained) {
+			return;
+		}
+#endif
 
 		// Check against any objects in this node
 		for (int i = 0; i < objects.Count; i++) {
